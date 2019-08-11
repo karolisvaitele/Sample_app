93b3320c98b2cc4eedb67ddc2b3e8b905475821b
Karolis <tavosnukis@gmail.com>
2019-08-11 13:10:18 +0300

    Updated users, can change their info, admins can delete other users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name,  presence: true, length: {maximum: 20}
     validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-    validates :password, presence: true, length: {minimum: 6, maximum: 255}
+    validates :password, presence: true, length: {minimum: 6, maximum: 255}, allow_nil: true
     has_secure_password
 
     # Returns the hash digest of the given string.
