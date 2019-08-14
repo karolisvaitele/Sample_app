82a89b17ff3a536b4827dcd80b60cd32a7e707f8
Karolis <tavosnukis@gmail.com>
2019-08-14 15:20:07 +0300

    Added microposts with possible image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
 
+    has_many :microposts, dependent: :destroy
+
     before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :name,  presence: true, length: {maximum: 20}
+    validates :name,  presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
     validates :password, presence: true, length: {minimum: 6, maximum: 255}, allow_nil: true
     has_secure_password
@@ -36,4 +38,9 @@ class User < ApplicationRecord
     def forget
         update_attribute(:remember_digest, nil)
     end
+
+    def feed
+        Micropost.where("user_id = ?", id)
+    end
+    
 end
