732e417ee3e2d19224e50cde8391806cbd8c1e7b
Karolis <tavosnukis@gmail.com>
2019-08-16 17:58:55 +0300

    Created follower feature, users can follow other users and see their posts on their feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
     attr_accessor :remember_token
 
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+    foreign_key: "follower_id",
+    dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+    foreign_key: "followed_id",
+    dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
 
     before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -39,8 +48,27 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
 
+    # Returns a user's status feed.
     def feed
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                        WHERE  follower_id = :user_id"
+                        Micropost.where("user_id IN (#{following_ids})
+                        OR user_id = :user_id", user_id: id)
+    end
+
+    # Follows a user.
+    def follow(other_user)
+        following << other_user
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+        following.include?(other_user)
     end
     
 end
